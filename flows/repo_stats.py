ec59127af25604d0261c2dab94044f31964ba794
mhidas <marty.hidas@utas.edu.au>
2023-08-29 15:04:46 +1000

    subflows

--- flows/repo_stats.py
+++ flows/repo_stats.py
@@ -12,14 +12,32 @@ def get_url(url: str, params: dict = None):
     return response.json()
 
 
+@flow
+def get_open_issues(repo_name: str, open_issues_count: int, per_page: int = 100):
+    issues = []
+    pages = range(1, -(open_issues_count // -per_page) + 1)
+    for page in pages:
+        issues.append(
+            get_url.submit(
+                f"https://api.github.com/repos/{repo_name}/issues",
+                params={"page": page, "per_page": per_page, "state": "open"},
+            )
+        )
+    return [i for p in issues for i in p.result()]
+
+
 @flow(log_prints=True, retries=3, retry_delay_seconds=5)
 def get_repo_info(repo_name: str = "PrefectHQ/prefect"):
     repo = get_url(f"https://api.github.com/repos/{repo_name}")
+    issues = get_open_issues(repo_name, repo["open_issues_count"])
+    issues_per_user = len(issues) / len(set([i["user"]["id"] for i in issues]))
+
+
     logger = get_run_logger()
     logger.info(f"{repo_name} repository statistics 🤓:")
     logger.info(f"Stars 🌠 : {repo['stargazers_count']}")
     logger.info(f"Forks 🍴 : {repo['forks_count']}")
-    print("'That's all for now!' he printed.")
+    logger.info(f"Average open issues per user 💌 : {issues_per_user:.2f}")
 
 
 if __name__ == "__main__":
