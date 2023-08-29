7b9c3e8e50e0f7317cd4fb16fb3372bae4855ccc
mhidas <marty.hidas@utas.edu.au>
2023-08-29 14:23:54 +1000

    add params, logging, retries

--- flows/repo_stats.py
+++ flows/repo_stats.py
@@ -1,17 +1,23 @@
+import sys
 import httpx
-from prefect import flow
+from prefect import flow, get_run_logger
 
 
-@flow
-def get_repo_info():
-    url = "https://api.github.com/repos/PrefectHQ/prefect"
+@flow(log_prints=True, retries=3, retry_delay_seconds=5)
+def get_repo_info(repo_name: str = "PrefectHQ/prefect"):
+    url = f"https://api.github.com/repos/{repo_name}"
     response = httpx.get(url)
     response.raise_for_status()
     repo = response.json()
-    print("PrefectHQ/prefect repository statistics 🤓:")
-    print(f"Stars 🌠 : {repo['stargazers_count']}")
-    print(f"Forks 🍴 : {repo['forks_count']}")
+    logger = get_run_logger()
+    logger.info(f"{repo_name} repository statistics 🤓:")
+    logger.info(f"Stars 🌠 : {repo['stargazers_count']}")
+    logger.info(f"Forks 🍴 : {repo['forks_count']}")
+    print("'That's all for now!' he printed.")
 
 
 if __name__ == "__main__":
-    get_repo_info()
+    try:
+        get_repo_info(sys.argv[1])
+    except IndexError:
+        get_repo_info()
